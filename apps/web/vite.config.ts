45a209bc01e8042c3cbe893ddae20bf3a0a70ed6
xenu <xenutheflock>
2026-06-26 23:09:46 +0800

    last

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: false,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
